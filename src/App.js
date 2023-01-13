63c3d7db311c3592ff3ceb0cf69fccdaaa83eb31
RaquelLecu <108267164+RaquelLecu@users.noreply.github.com>
2023-01-13 11:50:44 +0100

    start button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      
+      <Button text= 'See More' />
 
     </div>
   );
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import { BtnStyle } from './styles'
+
+const Button = ({ text }) => {
+    return (
+      <BtnStyle>{text}</BtnStyle>
+    )
+  }
+  
+  export default Button
