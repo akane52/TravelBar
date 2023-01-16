3795a45f6e771e811da1417e3735bdd7f1a4a264
akane52 <lulomo@bemen3.cat>
2023-01-16 10:32:28 +0100

    prueba2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Button from './components/Button'
 import CocktailCards from './components/CocktailCardComponent';
 import logo from "../src/resources/martinez.jpg"
 import NavBar from './components/NavBar';
+import FirstPagePhoto from './components/FirstPagePhoto/index.jsx';
 
 
 function App() {
   return (
     <Main className="App">
       <NavBar title='TravelBar' />
+      <FirstPagePhoto></FirstPagePhoto>
       <Button text= 'See More' />
       <CocktailCards
           picture={logo}
