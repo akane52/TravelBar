16e95c8b32837b237a184f4908f7877887c0b127
poliernus <polmeca@gmail.com>
2023-01-16 15:55:04 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import Button from './components/Button'
 import CocktailCards from './components/CocktailCardComponent';
 import logo from "../src/resources/martinez.jpg"
 import NavBar from './components/NavBar';
+import { useState,useEffect } from 'react';
+import Axios from 'axios';
 import TravelBarInformation from './components/InformationComponent/index'
 import barPhoto from "../src/resources/barPhoto.jpg"
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    Axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
+    .then(response => {
+      setData(response.data)
+    })
+  },[setData])
+
+  console.log(data)
+
+
   return (
     <Main className="App">
-      <NavBar title='TravelBar' />
+      <NavBar title='TravelBar' 
+      />
       <TravelBarInformation
         picture={barPhoto}
       ></TravelBarInformation>
