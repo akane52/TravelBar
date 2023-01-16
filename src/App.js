62b15358d87e45ca5cd093a93bbe753920d3452e
poliBCNGEEK <pol.melaracascales@travelport.com>
2023-01-16 12:22:06 +0100

    data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import Button from './components/Button'
 import CocktailCards from './components/CocktailCardComponent';
 import logo from "../src/resources/martinez.jpg"
 import NavBar from './components/NavBar';
+import { useState,useEffect } from 'react';
+import Axios from 'axios';
 
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    Axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
+    .then(response => {
+      setData(response.data)
+    })
+  },[setData])
+
+  console.log(data)
+
+
   return (
     <Main className="App">
       <NavBar title='TravelBar' />
