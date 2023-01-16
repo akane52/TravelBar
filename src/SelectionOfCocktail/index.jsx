dad1f0a2f6225672b3eff485f4599c10965b39ca
RaquelLecu <108267164+RaquelLecu@users.noreply.github.com>
2023-01-16 10:25:50 +0100

    start

---
+++ src/SelectionOfCocktail/index.jsx
@@ -0,0 +1,10 @@
+import { SelectionList } from './styles'
+import CocktailCards from '../CocktailCardComponent';
+
+const SelectionOfCocktails = ({ list }) => {
+    return (
+      <SelectionList></SelectionList>
+    )
+  }
+  
+  export default SelectionOfCocktails
